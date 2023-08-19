bc836a78dfd4e727d4a534c7518820d213889ed5
Md Sharif <srka780@gmail.com>
2023-08-19 20:08:24 +0600

    onkeyPass by loginPage update

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
+let email1 = document.getElementById("email_put")
+let password1 = document.getElementById("password_put")
 function validetion(){
     let email = document.getElementById("email_put").value
     let password = document.getElementById("password_put").value
     if(email === "" || password === ""){
         document.getElementById("error_pop").style.display = "flex"
-        closetimeOut("object")
+        closetimeOut()
     }else{
         if(email !== "sharif@gmail.com"){
             document.getElementById("error_img").src = ".././img/emailerror.png"
@@ -14,7 +16,8 @@ function validetion(){
             document.getElementById("error_img").src = ".././img/passerror.png"
             document.getElementById("error_text").innerHTML = "Please Chack You'r Password <br> invalid Email"
             document.getElementById("error_pop").style.display = "flex"
-            closetimeOut("object")
+            //closetimeOut()
+            
         }else{
             window.location.href = "./banking.html"
         }
@@ -23,6 +26,28 @@ function validetion(){
 document.getElementById('submit').addEventListener("click", () => {
     validetion()
 })
+
+email1.addEventListener('keyup', (e) =>{
+    if(e.key === "Enter"){
+        if(email1.value == ""){
+            validetion()
+            closetimeOut("object")
+        }else{
+            password1.focus()
+        }
+    }
+})
+password1.addEventListener('keyup', (e) =>{
+    if(e.key === "Enter"){
+        if(password1.value == ""){
+            validetion()
+            closetimeOut()
+        }else{
+            document.getElementById('submit').click()
+        }
+    }
+})
+
 closetimeOut = (x) => {
     document.getElementById("error_close").addEventListener("click", even => {
         if(typeof even.target == x){
